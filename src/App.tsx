187675cad7629b67dd420a51c61d6cae6b45c7a3
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-12-21 16:12:28 +0530

    Update routes and add Home and Login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<h1>Home</h1>} />
-				<Route path="/login" element={<h1>Login Page</h1>} />
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
 				<Route path="*" element={<h1>Not Found</h1>} />
 			</Routes>
 		</BrowserRouter>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="flex h-screen w-screen">
+      <div>right side</div>
+    </div>
+  );
+};
+
+export default Login;
