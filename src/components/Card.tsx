a105717519005bc8c6c1aeaa4bda5ce59c935e11
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-12-21 22:38:40 +0530

    Add Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { cn } from '../lib/utils';
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Card = ({ children, className }: Props) => {
+  return (
+    <div
+      className={cn(
+        'card-shadow flex flex-col items-center justify-center rounded-3xl',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
